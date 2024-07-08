5eb442e79a169ea1691c88a3063f247e3510b63e
maegon <dlagorhs99@naver.com>
2024-07-08 17:47:23 +0900

    product detail view

--- src/main/java/com/code/market/product/controller/ProductController.java
+++ src/main/java/com/code/market/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +21,10 @@ public class ProductController {
         List<Product> productLit = productService.getList();
         model.addAttribute("productList", productLit);
         return "product/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") long id, Model model) {
+        return "product/detail";
     }
 }
