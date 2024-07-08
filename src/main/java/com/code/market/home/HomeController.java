9dcd88a1cad8e13e348434d2706bcdbb577e4445
maegon <dlagorhs99@naver.com>
2024-07-08 15:20:47 +0900

    HomeController test

---
+++ src/main/java/com/code/market/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.code.market.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "홈 화면입니다.";
+    }
+}
