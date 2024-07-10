97e99d062347e64c1c9eccd19727f6f7dc62429b
maegon <dlagorhs99@naver.com>
2024-07-10 17:20:17 +0900

    question MVC complete

--- src/main/java/com/code/market/question/controller/QuestionController.java
+++ src/main/java/com/code/market/question/controller/QuestionController.java
@@ -1,21 +1,36 @@
 package com.code.market.question.controller;
 
+import com.code.market.member.entity.Member;
+import com.code.market.member.service.MemberService;
+import com.code.market.product.entity.Product;
+import com.code.market.product.service.ProductService;
 import com.code.market.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionService;
+    private final ProductService productService;
+    private final MemberService memberService;
 
-    @PostMapping("/create")
-    public String create() {
-        questionService.create();
+    @PostMapping("/create/{id}")
+    public String create(@PathVariable("id") Long id,
+                         Principal principal,
+                         @RequestParam("content") String content) {
+        Product product = productService.getProduct(id);
+        Member member = memberService.findByUserName(principal.getName());
 
-        return "redirect:";
+        questionService.create(product, member, content);
+
+        return "redirect:/product/detail/" + id;
     }
 }
--- src/main/java/com/code/market/question/service/QuestionService.java
+++ src/main/java/com/code/market/question/service/QuestionService.java
@@ -1,5 +1,8 @@
 package com.code.market.question.service;
 
+import com.code.market.member.entity.Member;
+import com.code.market.product.entity.Product;
+import com.code.market.question.entity.Question;
 import com.code.market.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,7 +12,11 @@ import org.springframework.stereotype.Service;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public void create() {
-
+    public void create(Product product, Member member, String content) {
+        Question question = new Question();
+        question.setProduct(product);
+        question.setMember(member);
+        question.setContent(content);
+        questionRepository.save(question);
     }
 }
