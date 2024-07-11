cafc9ac0c661d3414eddb79ef5fd2c36e5aeecfa
maegon <dlagorhs99@naver.com>
2024-07-11 15:28:04 +0900

    modify, delete, create authority

--- src/main/java/com/code/market/question/controller/QuestionController.java
+++ src/main/java/com/code/market/question/controller/QuestionController.java
@@ -7,9 +7,12 @@ import com.code.market.product.service.ProductService;
 import com.code.market.question.entity.Question;
 import com.code.market.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -21,6 +24,7 @@ public class QuestionController {
     private final ProductService productService;
     private final MemberService memberService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String create(@PathVariable("id") Long id, Principal principal, @RequestParam("content") String content) {
         Product product = productService.getProduct(id);
@@ -31,18 +35,35 @@ public class QuestionController {
         return "redirect:/product/detail/" + id;
     }
 
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String modify(@PathVariable("id") Long id, Model model) {
+    public String modify(@PathVariable("id") Long id, Model model, Principal principal) {
         Question question = questionService.getQuestion(id);
+
+        // 로그인은 됐는데 작성자 주인이 아니면 못하게 막기
+        if(question.getMember().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
         model.addAttribute("question", question);
         return "question/modify";
     }
 
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id) {
+    public String delete(@PathVariable("id") Long id, Principal principal) {
         Question question = questionService.getQuestion(id);
         questionService.delete(question);
         long productId = question.getProduct().getId();
+
+        // 로그인은 됐는데 작성자 주인이 아니면 못하게 막기
+        if(question.getMember().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+
         return "redirect:/product/detail/" + productId;
     }
 
