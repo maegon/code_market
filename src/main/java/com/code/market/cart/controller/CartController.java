2836c076bbb15652d36714060005d0a48082c405
maegon <dlagorhs99@naver.com>
2024-07-12 14:46:52 +0900

    add cart entity, service, repository & edit detail page

---
+++ src/main/java/com/code/market/cart/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.code.market.cart.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+public class CartController {
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/list")
+    public String list() {
+        return "cart/list";
+    }
+}
---
+++ src/main/java/com/code/market/cart/entity/Cart.java
@@ -0,0 +1,12 @@
+package com.code.market.cart.entity;
+
+import com.code.market.base.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Cart extends BaseEntity {
+}
