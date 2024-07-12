e34a9c60ac1b187fda90d116e6e07cfefecb8126
maegon <dlagorhs99@naver.com>
2024-07-12 15:24:34 +0900

    add cart feature

--- src/main/java/com/code/market/cart/controller/CartController.java
+++ src/main/java/com/code/market/cart/controller/CartController.java
@@ -1,18 +1,42 @@
 package com.code.market.cart.controller;
 
+import com.code.market.cart.service.CartService;
+import com.code.market.member.entity.Member;
+import com.code.market.member.service.MemberService;
+import com.code.market.product.entity.Product;
+import com.code.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
+    private final CartService cartService;
+    private final ProductService productService;
+    private final MemberService memberService;
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
     public String list() {
         return "cart/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add/{id}")
+    public String add(@PathVariable("id") Long id, Principal principal) {
+        Product product = productService.getProduct(id);
+        Member member = memberService.findByUserName(principal.getName());
+
+        cartService.add(product, member);
+
+        return "redirect:/cart/list";
     }
 }
--- src/main/java/com/code/market/cart/service/CartService.java
+++ src/main/java/com/code/market/cart/service/CartService.java
@@ -1,10 +1,27 @@
 package com.code.market.cart.service;
 
+import com.code.market.cart.entity.Cart;
+import com.code.market.cart.repository.CartRepository;
+import com.code.market.member.entity.Member;
+import com.code.market.product.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
+
+    public void add(Product product, Member member) {
+        Cart c = new Cart();
+        c.setProduct(product);
+        c.setMember(member);
+        c.setCreateDate(LocalDateTime.now());
+
+        cartRepository.save(c);
+    }
 
 }
