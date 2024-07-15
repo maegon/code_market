5a5987e8f11408c9a6800314ea567fd025671b24
maegon <dlagorhs99@naver.com>
2024-07-15 14:44:19 +0900

    admin productController test & create page form

---
+++ src/main/java/com/code/market/product/controller/AdmProductController.java
@@ -0,0 +1,21 @@
+package com.code.market.product.controller;
+
+import com.code.market.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/adm/product")
+public class AdmProductController {
+    private final ProductService productService;
+
+    @GetMapping("/create")
+    public String create() {
+        return "admin/product/create";
+    }
+
+
+}
